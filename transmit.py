2ab76a0729c30214f7a4d2f3a9acdbef8dfd2f62
virgesmith <andrew@friarswood.net>
2021-07-18 16:55:35 +0100

    make more robust

--- transmit.py
+++ transmit.py
@@ -3,6 +3,8 @@ import os
 from time import sleep
 import disruptive as dt
 
+import logging
+
 import dt_adapter
 
 def main():
@@ -16,31 +18,33 @@ def main():
 
   pisensehat = dt_adapter.PiSenseHat()
 
+  label_filters={"virtual-sensor": "", "type": dt_adapter.SENSOR_NAME, "external_id": dt_adapter.get_device_id()}
   while True:
+    try:
+      vsensors = dt.Device.list_devices(project_id, label_filters=label_filters)
 
-    label_filters={"virtual-sensor": "", "type": dt_adapter.SENSOR_NAME, "external_id": dt_adapter.get_device_id()}
-    vsensors = dt.Device.list_devices(project_id, label_filters=label_filters)
+      reading = pisensehat.read()
+      status_msg = f"sensor status={reading['status']} @ {reading['timestamp']}"
+      logging.info(status_msg)
 
-    reading = pisensehat.read()
-    status_msg = f"sensor status={reading['status']} @ {reading['timestamp']}"
-    print(status_msg)
-
-    for vs in vsensors:
-      if reading["status"] == "OK":
-        if vs.labels["virtual-sensor"] == "pressure" and vs.device_type == "temperature":
-          data = dt.events.Temperature(celsius=reading["pressure"], timestamp=reading["timestamp"])
-        elif vs.labels["virtual-sensor"] == "humidity" and vs.device_type == "humidity":
-          data = dt.events.Humidity(celsius=reading["temperature"], relative_humidity=reading["humidity"], timestamp=reading["timestamp"])
+      for vs in vsensors:
+        if reading["status"] == "OK":
+          if vs.labels["virtual-sensor"] == "pressure" and vs.device_type == "temperature":
+            data = dt.events.Temperature(celsius=reading["pressure"], timestamp=reading["timestamp"])
+          elif vs.labels["virtual-sensor"] == "humidity" and vs.device_type == "humidity":
+            data = dt.events.Humidity(celsius=reading["temperature"], relative_humidity=reading["humidity"], timestamp=reading["timestamp"])
+          else:
+            continue
+          print("updating %s" % vs.device_id)
+          err = dt.Emulator.publish_event(vs.device_id, project_id, data=data)
+          assert not err, str(err)
         else:
-          continue
-        print("updating %s" % vs.device_id)
-        err = dt.Emulator.publish_event(vs.device_id, project_id, data=data)
-        assert not err, str(err)
-      else:
-        err = dt.Device.set_label(vs.device_id, project_id, "error", status_msg)
-        assert not err, str(err)
+          err = dt.Device.set_label(vs.device_id, project_id, "error", status_msg)
+          assert not err, str(err)
 
-    sleep(dt_adapter.UPDATE_PERIOD)
+      sleep(dt_adapter.UPDATE_PERIOD)
+    except Exception as e:
+      logging.error(f"{e.__class__.__name__}: {str(e)}")
 
 
 if __name__ == "__main__":
